5fd5053377abc56126f0ce81914a8f522e8959df
Hachiikoo <scruz5114@gmail.com>
2022-02-03 13:59:31 -0300

    'scriptAtualizado'

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ function carregar (){
     var img = window.document.getElementById("imagem")
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = 'agora são',hora
+    
+    msg.innerHTML ='Agora são: '+hora+' horas'
     if (hora>=0 && hora<12){
     //Bom dia!
+    img.src = 'manha.png'
     }
-    else if(hora>=12 && hora < 18){
+    else if(hora>=12 && hora <= 18){
         //boa tarde
+        img.src = 'tarde.png'
     }
     else {
         // boa noite
+        img.src = 'noite.png'
     }
     }
-}
+
